6f69faff1d1431237db6da716138d47b79279dca
Junjie Luo <jjluo@umd.edu>
2021-09-11 00:47:33 -0400

    update headers

--- doctor_reviews/doctor_vitals.py
+++ doctor_reviews/doctor_vitals.py
@@ -20,6 +20,8 @@ import random
 #     # "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4,zh-TW;q=0.2",
 # }
 
+# HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36',}
+
 HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36',}
 
 
